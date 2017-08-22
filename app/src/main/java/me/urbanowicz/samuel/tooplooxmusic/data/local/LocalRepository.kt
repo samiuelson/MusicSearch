a969307962e99d583f474781280b78a4702754e2
samuel.urbanowicz <samiuelson@gmail.com>
2017-08-22 23:11:23 +0200

    Optimize LocalRepository::get() function

--- app/src/main/java/me/urbanowicz/samuel/tooplooxmusic/data/local/LocalRepository.kt
+++ app/src/main/java/me/urbanowicz/samuel/tooplooxmusic/data/local/LocalRepository.kt
@@ -1,7 +1,6 @@
 package me.urbanowicz.samuel.tooplooxmusic.data.local
 
 import android.util.Log
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializer
 import me.urbanowicz.samuel.tooplooxmusic.data.Repository
@@ -11,31 +10,33 @@ import me.urbanowicz.samuel.tooplooxmusic.extensions.fromJson
 class LocalRepository(private val json: String): Repository.Simple<Song> {
     private val TAG = "LocalRepository"
 
-    private val gson: Lazy<Gson>
+    private val songs: Lazy<Collection<Song>>
 
     init {
-        gson = lazy {
-            GsonBuilder().registerTypeAdapter(Int::class.java, JsonDeserializer<Int> { element, _, _ ->
-                var result: Int = Int.MIN_VALUE
-                element?.let {
-                    if (!element.isJsonPrimitive) {
-                        result = Int.MIN_VALUE
-                    } else {
-                        try {
-                            result = element.asJsonPrimitive.asNumber.toInt()
-                        } catch (e: NumberFormatException) {
-                            Log.d(TAG, "Received bad number format. Ignoring \"${element.asString}\" value.")
+        songs = lazy {
+            val gson =
+                GsonBuilder().registerTypeAdapter(Int::class.java, JsonDeserializer<Int> { element, _, _ ->
+                    var result: Int = Int.MIN_VALUE
+                    element?.let {
+                        if (!element.isJsonPrimitive) {
+                            result = Int.MIN_VALUE
+                        } else {
+                            try {
+                                result = element.asJsonPrimitive.asNumber.toInt()
+                            } catch (e: NumberFormatException) {
+                                Log.d(TAG, "Received bad number format. Ignoring \"${element.asString}\" value.")
+                            }
                         }
                     }
-                }
-                result
-            }).create()
+                    result
+                }).create()
+            val songs: Collection<LocalSong> = gson.fromJson(json)
+            songs
         }
     }
 
     override fun get(): Collection<Song> {
-        val songs: Collection<LocalSong> = gson.value.fromJson(json)
-        return songs
+        return songs.value
     }
 
 }
